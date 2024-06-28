773519d0263ed462d929f0085a412ad777d0f24c
sunombre1 <suemail1@gmail.com>
2024-06-28 01:05:16 -0500

    Siete: Acceder a ruta protegida

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const { register, login } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+// Ruta protegida de ejemplo
+router.get('/protected', authMiddleware, (req, res) => {
+    res.status(200).json({ mensaje: 'Accediste a una ruta protegida', user: req.user });
+  });
 
 module.exports = router;
